cb83f2ad905ddc23310ee0fd5b1e60d395072e87
Prince2411Anchanto <prince.mittal@anchanto.com>
2021-07-08 10:14:12 +0530

    added columns of timestamp to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_043734) do
+ActiveRecord::Schema.define(version: 2021_07_08_044149) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "desciption"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
