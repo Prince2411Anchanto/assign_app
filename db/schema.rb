93611c9ae1332c2b23010d456d1c586ee3a4fcd7
Prince2411Anchanto <prince.mittal@anchanto.com>
2021-07-08 11:27:26 +0530

    edited spelling of description

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_044149) do
+ActiveRecord::Schema.define(version: 2021_07_08_055458) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "desciption"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
