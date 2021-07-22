1896b369a4705f14d925d9a99815851fa297383c
Prince2411Anchanto <prince.mittal@anchanto.com>
2021-07-22 15:35:15 +0530

    Added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+    has_many :articles
     validates :username, uniqueness: { case_sensitive: false },
                 presence: true, 
                 length: { minimum: 3, maximum: 25 }
-    VALID_EMAIL_FORMAT = /\A[a-zA-Z0-9]+\@[A-Za-z0-9]+\.[a-z]+\z/i
+    VALID_EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, uniqueness: { case_sensitive: false },
                 presence: true, 
                 length: { maximum: 100 },
