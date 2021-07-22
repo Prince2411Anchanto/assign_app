091af795662238ef051513864c8aed8bcd86d503
Prince2411Anchanto <prince.mittal@anchanto.com>
2021-07-22 14:50:14 +0530

    added user validatiions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-    
+    validates :username, uniqueness: { case_sensitive: false },
+                presence: true, 
+                length: { minimum: 3, maximum: 25 }
+    VALID_EMAIL_FORMAT = /\A[a-zA-Z0-9]+\@[A-Za-z0-9]+\.[a-z]+\z/i
+    validates :email, uniqueness: { case_sensitive: false },
+                presence: true, 
+                length: { maximum: 100 },
+                format: { with: VALID_EMAIL_FORMAT }
 end
