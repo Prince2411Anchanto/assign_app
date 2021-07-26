f98eb5b4c74d5ae1eab9e10437c556a6ca592f97
Prince2411Anchanto <prince.mittal@anchanto.com>
2021-07-26 11:04:16 +0530

    Added categories field with admin access using test driven development

---
+++ test/integration/create_category_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @category = Category.create(name: "Sports")
+    @admin_user = User.create(username: "Prince", email: "pm@gmail.com", password: "p@1234", admin: true)
+  end
+  test "get new category form and create category" do
+    sign_in_as(@admin_user)
+    get '/categories/new'
+    assert_response :success
+    assert_difference('Category.count', 1) do
+      post categories_url, params: { category: { name: "Travel" } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "Travel", response.body
+  end
+  
+  test "get new category form and reject invalid category" do
+    sign_in_as(@admin_user)
+    get '/categories/new'
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_url, params: { category: { name: " " } }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+end
